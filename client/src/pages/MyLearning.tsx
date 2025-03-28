6c84deae1456d7b78e48950ea79ab8d0d00a3ef6
raulchavezjr7 <raulchavezjr7@gmail.com>
2025-03-28 16:00:31 -0500

    added video

--- client/src/pages/MyLearning.tsx
+++ client/src/pages/MyLearning.tsx
@@ -1,3 +1,29 @@
+import { useState } from "react";
+import "./MyLearning.css";
+
 export const MyLearning = () => {
-  return <div>MyLearning</div>;
+  const [controls, setControls] = useState(false);
+  const video = "video-3.mp4";
+
+  const handleControls = (control: boolean) => {
+    setControls(control);
+  };
+
+  return (
+    <>
+      <div>MyLearning</div>
+      <video
+        className="learning-video"
+        controls={controls}
+        autoPlay={true}
+        disablePictureInPicture={true}
+        onMouseEnter={() => handleControls(true)}
+        onMouseLeave={() => {
+          handleControls(false);
+        }}
+      >
+        <source src={`/video/${video}`} type="video/mp4" />
+      </video>
+    </>
+  );
 };
